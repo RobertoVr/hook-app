2374f8704e4aa4266214c5fc65d040e6203a0b19
Roberto Valdez <robert.tito94@gmail.com>
2020-11-06 11:33:16 -0600

    test add todo

--- src/test/components/08-useReducer/TodoAdd.test.js
+++ src/test/components/08-useReducer/TodoAdd.test.js
@@ -11,7 +11,7 @@ describe('Pruebas en TodoAdd', () => {
         <TodoAdd
             handleAddToDo={handleAddToDo}
         />
-    )
+    );
 
     test('Debe mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
@@ -22,6 +22,23 @@ describe('Pruebas en TodoAdd', () => {
         expect(handleAddToDo).toHaveBeenCalledTimes(0);
 
     });
+    test('Debe de llamar la funcion handleAddTodo', () => {
+        const value = 'aprender JS';
 
-
-})
+        wrapper.find('input').simulate('change', {
+            target: {
+                name: 'description',
+                value
+            }
+        });
+        wrapper.find('form').simulate('submit', { preventDefault() { } });
+        expect(handleAddToDo).toHaveBeenCalledTimes(1);
+        expect(handleAddToDo).toHaveBeenCalledWith(expect.any(Object));
+        expect(handleAddToDo).toHaveBeenCalledWith({
+            id: expect.any(Number),
+            desc: value,
+            done: false
+        });
+        expect(wrapper.find('input').prop('value')).toBe('');
+    });
+});
